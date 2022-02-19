2c7c71b6b30304ba229e1eca1971d95ef93153bd
LuizLLPz <luizllp321@gmail.com>
2022-02-18 21:45:22 -0300

    Consumo da api implementado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { Header } from './components/Header';
+import { FilterSystem } from './components/FilterSystem'
+import { FlagsComponent } from './components/FlagsComponent'
+
 import './styles.css';
 
 export const ThemeContext = React.createContext(null);
 
 function App() {
   const [theme, setTheme] = useState('light');
+  const [data, setData] = useState();
+  useEffect(async() => {
+     let resp = await fetch("https://restcountries.com/v3.1/all"); 
+     const data = await resp.json();
+     setData(data);
+  },[]);
+  
+
   return (
     <ThemeContext.Provider value={{theme, setTheme}}>
       <div className={'app ' + (theme == 'light' ? 'app-light' :  'app--dark')}>
         <Header/>
-      </div>
+        <FilterSystem/>
+        <FlagsComponent data={data}/>
+      </div> 
     </ThemeContext.Provider>
   );
 }
