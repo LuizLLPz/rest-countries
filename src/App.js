7e6ffc7580a86c7009114825c773fbb0f59791d8
LuizLLPz <luizllp321@gmail.com>
2022-02-18 21:10:30 -0300

    Criação de estilos básicos e adição de theme switcher

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Header } from './components/Header';
+import './styles.css';
+
+export const ThemeContext = React.createContext(null);
 
 function App() {
- 
+  const [theme, setTheme] = useState('light');
   return (
-    <div className="App">
-    </div>
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <div className={'app ' + (theme == 'light' ? 'app-light' :  'app--dark')}>
+        <Header/>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
